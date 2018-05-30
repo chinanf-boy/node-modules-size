e2e25902915361fcb1ff0eff5527f48ca3f248b7
chinanf-boy <865501259@qq.com>
2018-05-30 18:34:03 +0800

    Done cli.js

--- cli.js
+++ cli.js
@@ -10,7 +10,21 @@ const cli = meow(`
 `);
 
 (async function run() {
+	const {twoLog} = require('two-log');
+	const chalk = require('chalk');
+
+	let l = twoLog(cli.flags['D'])
+	l.start('search node_modules ...',{log: 'info'})
+
 	let cwd = cli.input[0] ? cli.input[0] : process.cwd()
 	let r  = await nodeModulesSize(cwd)
-	console.log(r)
+
+	if(r){
+		l.stop("good", {ora: 'succeed'})
+		Object.keys(r).forEach(p =>{
+			console.log(p,chalk.green(r[p]))
+		})
+	}else{
+		l.stop("error", {ora: 'fail'})
+	}
 })()
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const dirsList = require('dirs-list');
 const execa = require('execa');
 const prettyBytes = require('pretty-bytes');
+const {loggerText} = require('two-log')
 
 const nodeModuleSize = async function(cwd){
 	let nodeModulesPath = await dirsList(cwd, ["*node_modules"])
@@ -11,6 +12,7 @@ const nodeModuleSize = async function(cwd){
 	if(nodeModulesPath.length){
 		for(let p of nodeModulesPath){
 			let pathResult = await execa("du", ['-s', p]).then(r =>r.stdout)
+			loggerText("got size "+pathResult)
 			rs.push(pathResult)
 		}
 	}
@@ -18,7 +20,7 @@ const nodeModuleSize = async function(cwd){
 	let pathSizes = {}
 
 	if(rs.length){
-
+		loggerText("recale size...")
 		for(let r of rs){
 			let [size, path] = r.split('\t')
 			if(size && path){
